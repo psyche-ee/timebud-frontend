59646fe7af2aa324679750c85d00e2698ec3ef1b
psyche-ee <eduardojr.belda@ctu.edu.ph>
2026-03-11 17:11:54 +0800

    add /api in axios.ts baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://timebud-backend.onrender.com",
+  baseURL: "https://timebud-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
