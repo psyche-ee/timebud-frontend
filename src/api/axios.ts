c3582157a230caf2b68bbf266107ba3ed9c5877b
psyche-ee <eduardojr.belda@ctu.edu.ph>
2026-03-29 10:35:12 +0800

    update axios baseUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://timebud-backend.onrender.com/api",
+  baseURL: "https://timebud-backend-3tyx.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
